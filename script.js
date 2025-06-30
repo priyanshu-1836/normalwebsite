19e62921087666f308c4d99bd8f2f1afae5250fb
priyanshu-1836 <rishikesh21092012@gmail.com>
2025-06-30 10:46:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ function sayHello() {
 function showAlert() {
   document.getElementById("alertBox").style.display = "block";
 }
-function showAlert() {
-  document.getElementById("alertBox").style.display = "block";
-}
-
 function closeAlert() {
   document.getElementById("alertBox").style.display = "none";
 }
